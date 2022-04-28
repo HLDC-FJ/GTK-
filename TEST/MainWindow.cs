3a771c67fc50b0c9b63b5eda32c22542404232f8
HLDC-FJ <yoshio-fujinuki@hldc.co.jp>
2022-04-28 17:11:13 +0900

    Monodevelop TEST Code

---
+++ TEST/MainWindow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Gtk;
+
+using System.Text;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Drawing;
+using System.Linq;
+
+
+
+
+public partial class MainWindow : Gtk.Window
+{
+    public SerialPort spp = new SerialPort();
+    public IList<string> ComList = new List<string>();
+
+    #region Init
+    private void SerialPortSerch()
+    {
+        int i = 0;
+        // Serial Port Serch
+        string[] splist = SerialPort.GetPortNames();
+        ComList.Clear();
+        i = combobox1.RowSpanColumn;
+
+        foreach (string pd in splist)
+        {
+            ComList.Add(pd);
+            string bbb = pd.Substring(4);
+            combobox1.AppendText(bbb);
+        }
+
+    }
+
+
+
+    private void ItemInit()
+    {
+        SerialPortSerch();
+    }
+    #endregion
+
+
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        ItemInit();
+    }
+
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButton1Clicked(object sender, EventArgs e)
+    {
+        int hoge = combobox1.Data.Count;
+        for (int i=0; i<10; i++)
+        {
+        }
+    }
+}
